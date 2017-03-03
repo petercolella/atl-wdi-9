32a007bc79ec742f7e96d843d344631beead8ab1
petercolella <peter.joseph.colella@gmail.com>
2017-03-02 22:53:34 -0500

    not my best work

--- unit_01/w02d04/instructor_notes/homes/script.js
+++ unit_01/w02d04/instructor_notes/homes/script.js
@@ -1,57 +1,46 @@
-console.log('hey');
+$(function () {
+    
+    // Swaps out the 'btn-danger' and 'btn-success' classes on the #addHome button
+    $('#addHome').removeClass('btn-danger').addClass('btn-success');
 
-$(function() {
-  console.log('Everything is ready!');
+    // Creates a jQuery Object that wraps a brand new link tag
+    var $newLink = $('<br><br><a id="zillowLink" href="http://www.zillow.com">Visit Zillow.com<a>');
 
-  $('#addHome').removeClass('btn-danger').addClass('btn-success');
-  $('.jumbotron').addClass('text-center');
+    // Adds the $newLink from above after the header
+    var $header = $('#header');
+    $newLink.insertAfter($header);
 
-  var $newLink = $('<br><br><a id="zillowLink" href="http://www.zillow.com">Visit Zillow</a>')
+    // Adds the $newLink from above at the end of the body
+    var $body = $('body');
+    $newLink.appendTo('body');
+    
+    // Adds a click event handler to the #addHome button 
+    // $('#addHome').on('click', function ($event) {
+    //     alert($event);
+    //     console.log(this);
+    // });
 
-  $newLink.appendTo('body')
-
-  $('#zillowLink').attr('target', '_blank');
-
-  $('#addHome').click(function($event) {
-    console.log($event);
-    console.log(this)
-  })
-
-  var removeHome = function($event) {
-    console.log($event.target)
-    console.log(this)
-    if($($event.target).is('button')) {
-      $(this).fadeOut(1000, function() {
-        $(this).remove()
-      })
-    }
-  }
-
-  $('#homes tbody').on('click', 'tr', removeHome)
-
-  $('#addHome').click(function($event) {
-    var home = newHomes.pop();
-
-    var $homeTr = $(
-      `
-      <tr>
-        <td>$()</td>
-        <td>${home.sf}</td>
-        <td>${home.bedrooms}</td>
-        <td>${home.baths}</td>
-        <td>${home.price}</td>
-        <td><button class="btn btn-xs btn-danger">Remove</button></td>
-      </tr>
-      `
-    );
-
-    $homeTr.appendTo('tbody');
-  })
+    // Pops a message that a row is about to be deleted when ANY of the table rows are clicked
+    // Then fades the row out for 50 milliseconds before removing it entirely
+    $('#homes tbody').on('click', 'tr', function () {
+        alert('You are about to delete this row.');
+        $(this).fadeOut(50, function() {
+            $(this).remove();
+        });
+    });
+});
 
   var newHomes = [
-      {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
-      {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
-      {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
-      {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
-  ];
-})
+    {address: "27569 Cedarwood Drive", sf: "2,535", bedrooms: 3, baths: 2.5, price: "$496,500"},
+    {address: "316 Annandale Drive", sf: "1,326", bedrooms: 4, baths: 2, price: "$275,000"},
+    {address: "251 Grandview Road", sf: "3,800", bedrooms: 3, baths: 2, price: "$699,900"},
+    {address: "28571 Manitoba", sf: "2,960", bedrooms: 4, baths: 3.5, price: "$775,000"}
+];
+  
+  // var $newHome = $("<tr><td>" + newHomes[i].address + "</td><td>" + newHomes[i].sf + "</td><td>" + newHomes[i].bedrooms + "</td><td>" + newHomes[i].baths + "</td><td>" + newHomes[i].price + "</td></tr>");
+  var $tbody = $('tbody')
+  $('#addHome').on('click', function () {
+      for (i = 0; i < newHomes.length; i++) {
+          var $newHome = $("<tr><td>" + newHomes[i].address + "</td><td>" + newHomes[i].sf + "</td><td>" + newHomes[i].bedrooms + "</td><td>" + newHomes[i].baths + "</td><td>" + newHomes[i].price + "</td></tr>");
+          $newHome.appendTo('tbody');
+  }});
--- unit_01/w02d04/morning_exercise/index.js
+++ unit_01/w02d04/morning_exercise/index.js
@@ -6,6 +6,21 @@
 
 var makeChange = function(price, payment) {
   // Your Code Here
+  var changeInCents = (price - payment)*100;
+  var changeArray = []
+  // 	'Q' : 0,
+  // 	'D' : 0,
+  // 	'N' : 0,
+  // 	'P' : 0
+  // ];
+  var quarters = Math.round(changeInCents/25);
+  var dimes = Math.round((changeInCents - (quarters*25))/10);
+  var nickels = Math.round((changeInCents - ((quarters*25) + (dimes*10))/5);
+  var pennies = changeInCents - (quarters*25) - (dimes*10) - (nickels*5));
+  changeArray[0] = quarters;
+  changeArray[1] = dimes;
+  changeArray[2] = nickels;
+  changeArray[3] = pennies;
 };
 
 /// DO NOT EDIT BELOW THIS LINE ///
